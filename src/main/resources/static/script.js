a5eedb60c20f5318c77549c4b7372fcea20a9006
Rutzno <d.rutzno@gmail.com>
2024-09-13 01:03:01 +0000

    milestone 3

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,8 +1,34 @@
 /**
  * @author Mack_TB
  * @since 01/09/2024
- * @version 1.0.2
+ * @version 1.0.3
  */
+
+let inputMsg = document.getElementById("input-msg")
+let connectingElement = document.querySelector(".connecting")
+let stompClient = null
+
+function connect() {
+    let socket = new SockJS("/ws")
+    stompClient = Stomp.over(socket)
+    stompClient.connect({}, onConnected, onError)
+}
+
+function onConnected() {
+    stompClient.subscribe("/topic/public", onMessageReceived)
+    connectingElement.classList.add("hidden")
+}
+
+function onError() {
+    connectingElement.textContent = "Could not connect to WebSocket server. Please refresh this page to retry"
+    connectingElement.style.color = "red"
+    connectingElement.style.display = "block"
+}
+
+function onMessageReceived(payload) {
+    let message = JSON.parse(payload.body)
+    createNode(message.content)
+}
 
 function createNode(message) {
     let container = document.getElementById("messages")
@@ -15,13 +41,18 @@ function createNode(message) {
     messageTag.scrollIntoView({"behavior": "smooth"})
 }
 
-let sendMsgBtn = document.getElementById("send-msg-btn")
-sendMsgBtn.addEventListener("click",  (event) => {
+function sendMessage() {
     // event.preventDefault()
-    let inputMsg = document.getElementById("input-msg")
-    let message = inputMsg.value
-    if (message != null && message !== "") {
-        createNode(message)
+    let messageContent = inputMsg.value.trim()
+    if (messageContent && stompClient) {
+        let chatMessage = {
+            content: messageContent
+        }
+        stompClient.send("/app/chat.sendMessage", {}, JSON.stringify(chatMessage))
         inputMsg.value = ""
     }
-})
+}
+
+connect()
+let sendMsgBtn = document.getElementById("send-msg-btn")
+sendMsgBtn.addEventListener("click",  sendMessage)
