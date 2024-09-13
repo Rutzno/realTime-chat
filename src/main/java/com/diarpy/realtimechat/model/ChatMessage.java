eaeec9888e9c2e7edb5b0613233e292ded7e97dd
Rutzno <d.rutzno@gmail.com>
2024-09-13 01:12:14 +0000

    milestone 3

---
+++ src/main/java/com/diarpy/realtimechat/model/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.diarpy.realtimechat.model;
+
+import lombok.Data;
+
+/**
+ * @author Mack_TB
+ * @since 01/09/2024
+ * @version 1.0.3
+ */
+
+@Data
+public class ChatMessage {
+    private String content;
+}
