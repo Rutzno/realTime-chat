c6ac50be8284bdaac48e38e8a62d005f7e370355
Rutzno <d.rutzno@gmail.com>
2024-09-14 12:25:20 +0000

    milestone 5

--- src/main/java/com/diarpy/realtimechat/controller/ChatController.java
+++ src/main/java/com/diarpy/realtimechat/controller/ChatController.java
@@ -1,7 +1,9 @@
 package com.diarpy.realtimechat.controller;
 
 import com.diarpy.realtimechat.model.ChatMessage;
+import com.diarpy.realtimechat.model.User;
 import com.diarpy.realtimechat.service.ChatService;
+import com.diarpy.realtimechat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -11,20 +13,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Deque;
+import java.util.Set;
 
 /**
  * @author Mack_TB
  * @since 01/09/2024
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 @RestController
 public class ChatController {
     private final ChatService chatService;
+    private final UserService userService;
 
     @Autowired
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService, UserService userService) {
         this.chatService = chatService;
+        this.userService = userService;
     }
 
     @MessageMapping("/chat.sendMessage")
@@ -38,12 +43,22 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
                                SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        User user = new User();
+        user.setUsername(chatMessage.getSender());
+
+        headerAccessor.getSessionAttributes().put("username", user);
+
+        userService.addUser(user);
         return chatMessage;
     }
 
     @GetMapping("/api/messages/history")
     public Deque<ChatMessage> getMessageHistory() {
         return chatService.getMessages();
+    }
+
+    @GetMapping("/api/users")
+    public Set<User> getOnlineUsers() {
+        return userService.getUsers();
     }
 }
