016b037451e60547d0ce16b354e134b32f67d2d4
Rutzno <d.rutzno@gmail.com>
2024-09-14 12:04:53 +0000

    milestone 4

--- src/main/java/com/diarpy/realtimechat/controller/ChatController.java
+++ src/main/java/com/diarpy/realtimechat/controller/ChatController.java
@@ -1,23 +1,49 @@
 package com.diarpy.realtimechat.controller;
 
 import com.diarpy.realtimechat.model.ChatMessage;
+import com.diarpy.realtimechat.service.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Deque;
 
 /**
  * @author Mack_TB
  * @since 01/09/2024
- * @version 1.0.3
+ * @version 1.0.4
  */
 
-@Controller
+@RestController
 public class ChatController {
+    private final ChatService chatService;
+
+    @Autowired
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        chatService.addMessage(chatMessage);
         return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @GetMapping("/api/messages/history")
+    public Deque<ChatMessage> getMessageHistory() {
+        return chatService.getMessages();
     }
 }
--- src/main/java/com/diarpy/realtimechat/model/ChatMessage.java
+++ src/main/java/com/diarpy/realtimechat/model/ChatMessage.java
@@ -1,14 +1,54 @@
 package com.diarpy.realtimechat.model;
 
-import lombok.Data;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Mack_TB
  * @since 01/09/2024
- * @version 1.0.3
+ * @version 1.0.4
  */
 
-@Data
 public class ChatMessage {
+    private MessageType type;
+    private String sender;
     private String content;
+    private String createdAt;
+
+    public enum MessageType {CHAT, JOIN, LEAVE}
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        LocalDateTime formattedDate = ZonedDateTime.parse(createdAt).toLocalDateTime();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a | MMM dd");
+        this.createdAt = formattedDate.format(formatter);
+    }
 }
