597597d18d436e3dec38db65614208db720d48c9
Rutzno <d.rutzno@gmail.com>
2024-09-14 12:18:42 +0000

    milestone 4

--- src/main/java/com/diarpy/realtimechat/controller/WebSocketEventListener.java
+++ src/main/java/com/diarpy/realtimechat/controller/WebSocketEventListener.java
@@ -1,6 +1,8 @@
 package com.diarpy.realtimechat.controller;
 
 import com.diarpy.realtimechat.model.ChatMessage;
+import com.diarpy.realtimechat.model.User;
+import com.diarpy.realtimechat.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,14 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Mack_TB
  * @since 01/09/2024
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 @Component
@@ -22,6 +27,8 @@ public class WebSocketEventListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketEventListener.class);
     @Autowired
     private SimpMessageSendingOperations messagingTemplate;
+    @Autowired
+    private UserService userService;
 
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
@@ -29,15 +36,22 @@ public class WebSocketEventListener {
     }
 
     @EventListener
+    public void handleWebSocketSubscribeListener(SessionSubscribeEvent event) {
+        LOGGER.info("Received a new web socket subscribe event");
+    }
+
+    @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
-        String username = (String) accessor.getSessionAttributes().get("username");
+        User username = (User) accessor.getSessionAttributes().get("username");
         if (username != null) {
             LOGGER.info("Disconnected from user {}", username);
 
             ChatMessage chatMessage = new ChatMessage();
-            chatMessage.setSender(username);
+            chatMessage.setSender(username.getUsername());
             chatMessage.setType(ChatMessage.MessageType.LEAVE);
+            chatMessage.setCreatedAt(LocalDateTime.now());
+            userService.removeUser(username);
 
             messagingTemplate.convertAndSend("/topic/public", chatMessage);
         }
---
+++ src/main/java/com/diarpy/realtimechat/model/User.java
@@ -0,0 +1,19 @@
+package com.diarpy.realtimechat.model;
+
+/**
+ * @author Mack_TB
+ * @since 01/09/2024
+ * @version 1.0.5
+ */
+
+public class User {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
