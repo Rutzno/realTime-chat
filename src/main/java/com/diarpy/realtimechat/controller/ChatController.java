c7adae926d8a4880afc317e193bc2c81b985a964
Rutzno <d.rutzno@gmail.com>
2024-09-15 18:29:22 +0000

    milestone 7

--- src/main/java/com/diarpy/realtimechat/controller/ChatController.java
+++ src/main/java/com/diarpy/realtimechat/controller/ChatController.java
@@ -5,20 +5,16 @@ import com.diarpy.realtimechat.model.User;
 import com.diarpy.realtimechat.service.ChatService;
 import com.diarpy.realtimechat.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Deque;
-import java.util.List;
 import java.util.Set;
 
 /**
