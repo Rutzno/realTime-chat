5ed19b3851b6da83577d881b03c62597307b0437
Rutzno <d.rutzno@gmail.com>
2024-09-15 18:19:32 +0000

    milestone 7

--- src/main/java/com/diarpy/realtimechat/service/ChatService.java
+++ src/main/java/com/diarpy/realtimechat/service/ChatService.java
@@ -3,25 +3,43 @@ package com.diarpy.realtimechat.service;
 import com.diarpy.realtimechat.model.ChatMessage;
 import org.springframework.stereotype.Service;
 
-import java.util.Deque;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
  * @author Mack_TB
  * @since 01/09/2024
- * @version 1.0.4
+ * @version 1.0.7
  */
 
 @Service
 public class ChatService {
 
-    private final Deque<ChatMessage> messages = new ConcurrentLinkedDeque<>();
+    private final Deque<ChatMessage> publicMessages = new ConcurrentLinkedDeque<>();
+    private final Map<String, Deque<ChatMessage>> privateMessages = new ConcurrentHashMap<>();
 
     public void addMessage(ChatMessage message) {
-        messages.add(message);
+        if (message.getRecipient() == null) {
+            publicMessages.add(message);
+        } else {
+            String chatID = createChatID(message.getSender(), message.getRecipient());
+            privateMessages.computeIfAbsent(chatID, k -> new ConcurrentLinkedDeque<>()).add(message);
+        }
     }
 
-    public Deque<ChatMessage> getMessages() {
-        return messages;
+    public Deque<ChatMessage> getPublicMessages() {
+        return publicMessages;
+    }
+
+    public Deque<ChatMessage> getPrivateMessages(String sender, String recipient) {
+        String chatID = createChatID(sender, recipient);
+        return privateMessages.getOrDefault(chatID, new ConcurrentLinkedDeque<>());
+    }
+
+    private String createChatID(String sender, String recipient) {
+        List<String> users = Arrays.asList(sender, recipient);
+        Collections.sort(users);
+        return users.get(0) + "-" + users.get(1);
     }
 }
